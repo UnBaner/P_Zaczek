80a5f6d42863b6d50609eba47c96642ef4592f87
UnBaner <90259661+UnBaner@users.noreply.github.com>
2022-05-16 13:32:17 +0200

    Add files via upload

---
+++ Pytanie/Pytanie.h
@@ -0,0 +1,22 @@
+#ifndef PYTANIE_H
+#define PYTANIE_H
+#include <iostream>
+using namespace std;
+
+class Pytanie
+{
+    public:
+       string trescPytania;
+       string a;
+       string b;
+       string c;
+       string poprawna;
+       string odpowiedz;
+       int punkty,nrPytania;
+
+       void wczytajPytanie();
+       void zadajPytanie();
+       void sprawdz();
+};
+
+#endif // PYTANIE_H
